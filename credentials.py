5c2a8f06d7cf857954cac2065a3b56ab39f6ad72
TG-ToolsFree <136507675+TG-ToolsFree@users.noreply.github.com>
2023-06-13 21:30:12 +0200

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,8 @@
+# enter your API credentials below - generate at https://my.telegram.org/auth
+api_id = 123456789
+api_hash = "46sadasd8asd46a8sa4d6ad48"
+
+#THIS IS JUST A GENERIC API_ID AND API_HASH!
+
+
+
